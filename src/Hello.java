664c12c3db983764baf20acc65d1937a3f1ce22e
LYH <202025310210@stu.scau.edu.cn>
2021-10-16 21:14:45 +0800

    1

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+//        Scanner scanner = new Scanner();
+    }
+}
