f736e1833b8cacf00f44e978814e6e18e7b31346
LYH <202025310210@stu.scau.edu.cn>
2021-10-16 21:45:54 +0800

    new commit

---
+++ src/text/TestCode.java
@@ -0,0 +1,11 @@
+package text;
+
+/**
+ * @author Lyh
+ * @date 2021/10/16 21:44
+ */
+public class TestCode {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
